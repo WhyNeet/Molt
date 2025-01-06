54d6986fe75038695e546d96f2e2f389c0b1189e
WhyNeet <arc.software.x@gmail.com>
2025-01-06 18:42:37 +0300

    test(parser): add variable declaration test

--- crates/parser/tests/expression.rs
+++ crates/parser/tests/expression.rs
@@ -2,20 +2,21 @@ use ast::{
     expression::Expression,
     literal::{Literal, Number},
     operator::Operator,
+    statement::Statement,
 };
 use lexer::scanner::Scanner;
 use parser::Parser;
 
 #[test]
 fn expression_works() {
-    let input = r#"1 * 3 + 2"#;
+    let input = r#"1 * 3 + 2;"#;
     let tokens = Scanner::tokenize(input).collect();
 
     let tree = Parser::new(tokens).parse();
 
     assert_eq!(
-        tree,
-        Expression::Binary {
+        tree[0],
+        Statement::Expression(Expression::Binary {
             left: Box::new(Expression::Binary {
                 left: Box::new(Expression::Literal(Literal::Number(Number::Int32(1)))),
                 operator: Operator::Mul,
@@ -23,6 +24,23 @@ fn expression_works() {
             }),
             operator: Operator::Add,
             right: Box::new(Expression::Literal(Literal::Number(Number::Int32(2))))
+        })
+    );
+}
+
+#[test]
+fn variable_declaration_works() {
+    let input = r#"let x = 1;"#;
+    let tokens = Scanner::tokenize(input).collect();
+
+    let tree = Parser::new(tokens).parse();
+
+    assert_eq!(
+        tree[0],
+        Statement::VariableDeclaration {
+            name: "x".to_string(),
+            expr: Expression::Literal(Literal::Number(Number::Int32(1))),
+            annotations: vec![]
         }
-    )
+    );
 }
