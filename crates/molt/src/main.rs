0bcc6f62211ced883bfb937288b4c110ca3d1171
WhyNeet <arc.software.x@gmail.com>
2025-01-07 16:15:51 +0300

    feat(molt): add lexing and parsing to cli

--- crates/molt/src/main.rs
+++ crates/molt/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
+use lexer::scanner::Scanner;
+use parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = std::env::args().nth(1).expect("expected filename.");
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let tokens = Scanner::tokenize(&contents).collect();
+    let tree = Parser::new(tokens).parse();
+
+    println!("parsed tree:\n{tree:?}");
 }
