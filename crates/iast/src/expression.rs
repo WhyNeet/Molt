05497acfec7e55d3d96e9e061502cc9659dd617a
WhyNeet <arc.software.x@gmail.com>
2025-01-12 22:26:42 +0300

    feat(iast): add checked expression

--- crates/iast/src/expression.rs
+++ crates/iast/src/expression.rs
@@ -1,10 +1,12 @@
+use std::rc::Rc;
+
 use ast::literal::Type;
 
 use crate::effect::Effect;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Expression {
-    pub expr: ast::expression::Expression,
+    pub expr: Rc<ast::expression::Expression>,
     pub effects: Vec<Effect>,
     pub ty: Type,
 }
--- crates/iast/src/statement.rs
+++ crates/iast/src/statement.rs
@@ -1,7 +1,36 @@
+use std::rc::Rc;
+
+use crate::expression::Expression;
+use ast::{annotation::Annotation, literal::Type};
+
 use crate::effect::Effect;
 
 #[derive(Debug, PartialEq)]
 pub struct Statement {
-    pub stmt: ast::statement::Statement,
+    pub stmt: Rc<StatementKind>,
     pub effects: Vec<Effect>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum StatementKind {
+    Expression {
+        expr: Expression,
+        end_semi: bool,
+    },
+    VariableDeclaration {
+        name: String,
+        expr: Expression,
+        /// If type is not specified, infer it.
+        ty: Option<Type>,
+    },
+    FunctionDeclaration {
+        name: String,
+        block: Option<Expression>,
+        return_type: Type,
+        parameters: Vec<(String, Type)>,
+    },
+    Annotated {
+        annotations: Vec<Annotation>,
+        stmt: Rc<Statement>,
+    },
 }
