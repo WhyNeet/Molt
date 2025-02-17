6f07da25c95b40acf874d4242278bc37954c2c19
WhyNeet <arc.software.x@gmail.com>
2025-02-17 22:43:20 +0300

    feat(molt): add cli arguments

---
+++ crates/molt/src/args.rs
@@ -0,0 +1,22 @@
+use std::path::PathBuf;
+
+use clap::ValueEnum;
+
+#[derive(clap::Parser)]
+#[command(version, about)]
+pub struct MoltCliArgs {
+    pub filename: Option<PathBuf>,
+
+    #[arg(short, long, value_enum)]
+    pub format: Option<OutputFormat>,
+
+    #[arg(short, long)]
+    pub output: Option<String>,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum OutputFormat {
+    Object,
+    ASM,
+    Bitcode,
+}
--- crates/molt/src/main.rs
+++ crates/molt/src/main.rs
@@ -1,18 +1,25 @@
-use std::{fs, path::Path};
+mod args;
 
+use std::{fs, path::Path, process};
+
+use args::{MoltCliArgs, OutputFormat};
 use artifact::BuildArtifactGenerator;
 use checker::Checker;
+use clap::{CommandFactory, Parser};
 use irgen::IrEmitter;
 use lexer::scanner::Scanner;
 use lirgen::emitters::module::LirModuleEmitter;
-use parser::Parser;
 
 fn main() {
-    let filename = std::env::args().nth(1).expect("expected filename.");
-    let contents = fs::read_to_string(filename).unwrap();
+    let args = MoltCliArgs::parse();
+    let filename = args.filename.unwrap_or_else(|| {
+        MoltCliArgs::command().print_help().unwrap();
+        process::exit(0)
+    });
+    let contents = fs::read_to_string(&filename).unwrap();
 
     let tokens = Scanner::tokenize(&contents).collect();
-    let tree = Parser::new(tokens).parse();
+    let tree = parser::Parser::new(tokens).parse();
 
     let tree = Checker::new(tree).check();
 
@@ -25,8 +32,26 @@ fn main() {
 
     let artifact_builder = BuildArtifactGenerator::default();
 
-    let output_path = Path::new("output.o");
-    artifact_builder.produce_object_file(&module, output_path);
+    let output_format = args.format.unwrap_or(OutputFormat::Object);
+
+    let output_path = args.output.unwrap_or_else(|| {
+        format!(
+            "{}.{}",
+            filename.to_str().unwrap(),
+            match output_format {
+                OutputFormat::Object => "o",
+                OutputFormat::Bitcode => "bc",
+                OutputFormat::ASM => "s",
+            }
+        )
+    });
+    let output_path = Path::new(&output_path);
+
+    match output_format {
+        OutputFormat::Object => artifact_builder.produce_object_file(&module, output_path),
+        OutputFormat::Bitcode => artifact_builder.produce_bitcode_file(&module, output_path),
+        OutputFormat::ASM => artifact_builder.produce_asm_file(&module, output_path),
+    }
 
     println!("\nCompiled 1 artifact.");
 }
